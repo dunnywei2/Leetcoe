bc856de13a24dfe1915b3e6f53aae143c91beaf9
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-09-30 00:30:58 -0400

    Update code_v00.c_running 00

--- code_v00.c
+++ code_v00.c
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int> tReturn;
+        int diff=0;
+         std::unordered_map<int,int> tMap;
+        for(int i=0;i< nums.size();i++)
+        {
+            diff=target-nums.at(i);
+            
+          
+        }
+      return  tReturn;
+    }
+};
