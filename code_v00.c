9358b5263bbb87f886570106e5dbd41304fef3c7
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-09-30 17:52:55 -0400

    Update code_v00.c_change before testing

--- code_v00.c
+++ code_v00.c
@@ -5,12 +5,14 @@ public:
         vector<int> tReturn;
         int diff=0;
          std::unordered_map<int,int> tMap;
+        std::unordered_map<int,int>::iterator iterator_map;
         for(int i=0;i< nums.size();i++)
         {
             diff=target-nums.at(i);
-            if(tMap.find(diff))
+            if(iterator_map=tMap.find(diff))
             {
-
+                   tReturn.push(iterator_map->seond);
+                   tReturn.push(i);
             }else{
                 tMap.push(nums.at(i));
             }   
