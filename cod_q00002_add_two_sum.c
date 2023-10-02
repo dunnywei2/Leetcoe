dbe9d97877c3bd708f2f0dcb953f7fda2b7a9cee
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-10-02 19:26:41 -0400

    Update cod_q00002_add_two_sum.c
    start question2_00

--- cod_q00002_add_two_sum.c
+++ cod_q00002_add_two_sum.c
@@ -11,6 +11,15 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        std::vector<int> carry;
+       while( (l1->next!=NULL)||(l2->next!=NULL)
+           {
+               int val1=l1 ? NULL : 0 : l1->val;
+               int val2=l2 ? NULL : 0 : l2->val;
+               int sum=l1->val1+val2->val;
+               
+           }
+           
+       
     }
 };
