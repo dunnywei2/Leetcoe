cd4d88be3b8874441ed58b3cb3ee9f91d9c03357
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-10-12 17:26:34 -0400

    Update standard_q000003.c
    q3 continue

--- standard_q000003.c
+++ standard_q000003.c
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
+        std::vector<char> tBuffer;
+        for(int i=0;i<s.length();i++)
+        {
+           if(!tBuffer.empty()){     
+              std::vector<char>::iterator iter;
+              iter=tBuffer.find(s[i]);
+              iter=tBuffer.find(s[i]);
+              if(iter!=tBuffer.end())
+              {
+                  return true
+              }
+              
+           }
+            if(s[i]) 
+        }
+        return 0;
     }
 };
