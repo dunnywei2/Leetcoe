68d1a4a62f9d6d353db14caf3c7aeb37fd39ab3c
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-10-12 02:01:25 -0400

    Update cod_q00002_add_two_sum.c
    continue change q2

--- cod_q00002_add_two_sum.c
+++ cod_q00002_add_two_sum.c
@@ -18,7 +18,7 @@ public:
         carryV.push_back(0);
         ListNode* carryL;
         int counter=0;
-       while( (l1->next!=NULL)||(l2->next!=NULL))
+       while( (l1!=NULL)||(l2!=NULL))
         {
             int val1,val2;
             if(l1)
@@ -39,16 +39,9 @@ public:
                // int val1=l1 ? NULL : 0 : l1->val;
                // int val2=l2 ? NULL : 0 : l2->val;
             
-            int sum=0;
-            if(carryV.empty())
-            {
-                sum=val1+val2;
-            }
-            else
-            {
-                sum=val1+val2+carryV.at(counter);
+            int sum=val1+val2+carryV.at(counter);
 
-            }
+            
             
 
             //32+87
