3bd178f6846d68e17bffa771b1dc2196face44b8
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-10-16 00:40:24 -0400

    Update standard_q000003.c
    adding useful links

--- standard_q000003.c
+++ standard_q000003.c
@@ -1,3 +1,9 @@
+/*
+ *->https://www.geeksforgeeks.org/window-sliding-technique/
+ *->https://www.geeksforgeeks.org/sliding-window-maximum-maximum-of-all-subarrays-of-size-k/
+ *->about set->https://www.youtube.com/watch?v=682xT_TcKUg
+ */
+
 class Solution {
 protected:
    std::vector<char> vector;
