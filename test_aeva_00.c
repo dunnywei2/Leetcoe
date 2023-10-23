6777ba2e61ec97e0670fbc042534ccaca2ba4bb3
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-10-23 13:16:43 -0700

    Update test_aeva_00.c
    continue test_aeva

--- test_aeva_00.c
+++ test_aeva_00.c
@@ -1,9 +1,18 @@
+
+// XXXX XXX_ XXXX XXXX XXXX XXXX XXXX XXXX
+// MMMM MMM1 NNNN NNNN NNNN NNNN NNNN NNNN
+// where M is decimal and N is Integer for greater number
+// MMMM MMM1, where first M is sign 
 float convert_to_temp(uint32 input_vall)
 {
-   if(input_val=0x0010000)
+   uint32 tValue=0;
+   uint32 tSum=0;
+   double tSum=0;
+   if(input_val&0x0010000)
    {
       //extended
-   }
+     tValue=(input_val&0x001FFFF)
+     
    else{
         //standard
    } 
