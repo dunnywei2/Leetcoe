171d2eebb09055b94ed7205e4ef35c08c0678d8b
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-10-23 19:17:40 -0700

    Update standard_q000029_dvided_two_integers.c.c
    working on q000029

--- standard_q000029_dvided_two_integers.c.c
+++ standard_q000029_dvided_two_integers.c.c
@@ -1,3 +1,6 @@
+//hint ->https://www.youtube.com/watch?v=XKuFGEGt5zo
+//hint2 in java->https://www.youtube.com/watch?v=htX69j1jf5U
+
 class Solution {
 public:
     int divide(int dividend, int divisor) {
