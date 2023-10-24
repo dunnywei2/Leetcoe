8ac4561e7ac018903be3cd57c41cd0a6f46cf4b6
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-10-24 00:40:38 +0000

    Update test_aeva_00.c
    continue editing test_aeva

--- test_aeva_00.c
+++ test_aeva_00.c
@@ -1,11 +1,12 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define MASK 0x1U
-int32 shiftingL(int32 aValue)
+int32_t shiftingL(int32_t aValue)
 {
-   int32 tTemp=aValu>>24;
-   int32 tSum=0;
+   int32_t tTemp=aValu>>24;
+   int32_t tSum=0;
    int counter=0;
    while(counter<8)
    {
@@ -21,15 +22,15 @@ int32 shiftingL(int32 aValue)
 // MMMM MMM1 NNNN NNNN NNNN NNNN NNNN NNNN
 // where M is decimal and N is Integer for greater number
 // MMMM MMM1, where M is the decimal number. First M is decimal
-float convert_to_temp(uint32 input_vall)
+float convert_to_temp(int32_t input_vall)
 {
-   uint32 tValue=0;
+   int32_t tValue=0;
    float tSum=0;
    if(input_val&0x0010000)
    {
       //extended
      tValue=(input_val&0x001FFFF);
-     int32 tTemp=(input_val&0xFFF10000)>>24;
+     int32_t tTemp=(input_val&0xFFF10000)>>24;
       float tFloat=0;
      for(int i=0;i<7;i++)
      {
