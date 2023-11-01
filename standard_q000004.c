7c8d43942fca2e3e21b8463afedb59b75bb7a2a0
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-01 15:04:55 -0700

    Update standard_q000004.c
    add chinese explanation

--- standard_q000004.c
+++ standard_q000004.c
@@ -2,6 +2,7 @@
   *->https://www.youtube.com/watch?v=QhShXjq5TWE&pp=ygUPbGVldGNvZGUgIzQgQysr
   *->https://www.youtube.com/watch?v=juzJ6SmxovA&pp=ygUlbGVldGNvZGUgMyBtZWRpdW0gb2Ygc29ydGVkIGFycmF5IEMrKw%3D%3D
   *->贾考博 LeetCode 3
+  *->chinese explanation->https://www.bilibili.com/video/BV1xe4y1278v/?spm_id_from=333.337.search-card.all.click&vd_source=352d86e561ef82fd069e1251bfae8933
   *->data structure->https://www.bilibili.com/video/BV13g41157hK/?spm_id_from=333.337.search-card.all.click
   *->use binary search->https://www.youtube.com/watch?v=yD7wV8SyPrc
   */
