3d5b817cfa0dbd78985eb357e1ee90f17149bcba
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-01 10:55:07 -0700

    Update standard_q000004.c
    add data structure links->

--- standard_q000004.c
+++ standard_q000004.c
@@ -2,6 +2,7 @@
   *->https://www.youtube.com/watch?v=QhShXjq5TWE&pp=ygUPbGVldGNvZGUgIzQgQysr
   *->https://www.youtube.com/watch?v=juzJ6SmxovA&pp=ygUlbGVldGNvZGUgMyBtZWRpdW0gb2Ygc29ydGVkIGFycmF5IEMrKw%3D%3D
   *->贾考博 LeetCode 3
+  *->data structure->https://www.bilibili.com/video/BV13g41157hK/?spm_id_from=333.337.search-card.all.click
   */
 
 class Solution {
