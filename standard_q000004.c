6375990f93616efc03757eb914c2ad55d72d405b
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-01 10:45:06 -0700

    Update standard_q000004.c
    update

--- standard_q000004.c
+++ standard_q000004.c
@@ -1,6 +1,7 @@
 /*
   *->https://www.youtube.com/watch?v=QhShXjq5TWE&pp=ygUPbGVldGNvZGUgIzQgQysr
   *->https://www.youtube.com/watch?v=juzJ6SmxovA&pp=ygUlbGVldGNvZGUgMyBtZWRpdW0gb2Ygc29ydGVkIGFycmF5IEMrKw%3D%3D
+  *->贾考博 LeetCode 3
   */
 
 class Solution {
