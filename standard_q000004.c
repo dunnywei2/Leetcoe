6cd4bc7ad25400bc7d761d2087e2f91b73e3bab1
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-01 10:39:39 -0700

    Create standard_q000004.c
    adding youtube link

---
+++ standard_q000004.c
@@ -0,0 +1,10 @@
+/*
+  *->https://www.youtube.com/watch?v=QhShXjq5TWE&pp=ygUPbGVldGNvZGUgIzQgQysr
+  */
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
