916d46c563f6a35bd93a5a04b01666ad09f5746e
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-01 17:34:28 -0700

    Update standard_q000004.c
    find the better chinese explaination

--- standard_q000004.c
+++ standard_q000004.c
@@ -3,6 +3,7 @@
   *->https://www.youtube.com/watch?v=juzJ6SmxovA&pp=ygUlbGVldGNvZGUgMyBtZWRpdW0gb2Ygc29ydGVkIGFycmF5IEMrKw%3D%3D
   *->贾考博 LeetCode 3
   *->chinese explanation->https://www.bilibili.com/video/BV1xe4y1278v/?spm_id_from=333.337.search-card.all.click&vd_source=352d86e561ef82fd069e1251bfae8933
+  *->better chinese explaination->choose this one->https://www.bilibili.com/video/BV1Xv411z76J/?spm_id_from=333.337.search-card.all.click&vd_source=352d86e561ef82fd069e1251bfae8933
   *->data structure->https://www.bilibili.com/video/BV13g41157hK/?spm_id_from=333.337.search-card.all.click
   *->use binary search->https://www.youtube.com/watch?v=yD7wV8SyPrc
   */
