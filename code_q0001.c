38dca30e35a1f53c4d3c9f4ca6310b1b5e14b782
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2025-05-13 06:33:26 -0700

    Update code_q0001.c
    Java finalize

--- code_q0001.c
+++ code_q0001.c
@@ -58,18 +58,23 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> hashmap = new HashMap<>();
 
-        for(int i=0;i<numbs.length;i++)
+        for(int i=0;i<nums.length;i++)
         {
             
             int diff=target-nums[i];
-            if(hashmap.contain(diff))
+            //Case I: 9-2=7
+            //9-7=2
+            if(hashmap.containsKey(diff))
             {
-                return new int{hashmap.get(diff),i};
+                return new int[] {hashmap.get(diff),i};
+                //Case 1: return {index 0, 1}
             }
             else{
-                hashmap.put(diff,i);
+                hashmap.put(nums[i],i);
+                // Case 1 index 0, 2:0
             }
         }
+        return new int[]{};
         
     }
 }
