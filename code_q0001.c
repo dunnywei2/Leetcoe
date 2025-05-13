5564134e1a05ae50c2125d73306caae263c83a8d
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2025-05-12 23:16:51 -0700

    Update code_q0001.c
    change

--- code_q0001.c
+++ code_q0001.c
@@ -1,5 +1,5 @@
 /*
- *Java
+ *Java->brute force
  */
 
 class Solution {
@@ -16,5 +16,22 @@ class Solution {
         }
         return nums;
         
+    }
+}
+
+/*
+java hash map
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numMap = new HashMap<>(); // A mapping to store numbers and their indices
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i]; // Find the required number to reach the target
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i}; // Return indices of the complement and current number
+            }
+            numMap.put(nums[i], i); // Store the number with its index
+        }
+        return new int[]{}; // This line is never reached due to the problem guarantee
     }
 }
