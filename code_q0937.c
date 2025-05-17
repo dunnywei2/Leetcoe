131d6ac50794d9c772dcdae8706869a6f627de21
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2025-05-17 04:21:04 -0700

    Update code_q0937.c
    adding the original one

--- code_q0937.c
+++ code_q0937.c
@@ -1,3 +1,71 @@
+->link with youtube->https://www.youtube.com/watch?v=UbNU98fKV8o
+
+  ->second one->https://www.youtube.com/watch?v=JRm0ybcPSFc
+
+
+
+  /*Other C++ soluction*/
+  class Solution {
+private:
+    string findKey(string &log) {
+        int i = 0;
+        string key = "";
+        for(char &ch: log) {
+            if(ch == ' ')
+                break;
+            key += ch;
+        }
+        return key;
+    }
+    string findContent(string &log) {
+        int i = 0;
+        while(i < log.size() && log[i] != ' ')
+            i++;
+
+        return log.substr(i);
+    }
+    bool isLetterLog(string &log) {
+        return log[log.size() - 1] >= 'a' 
+                && log[log.size() - 1] <= 'z';
+    }
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        vector<string> letterLogs;
+        // O(N)
+        for(string &log: logs) {
+            if(isLetterLog(log)) 
+                letterLogs.push_back(log);
+        }
+
+        // O(NLogN ∗ L)
+        sort(letterLogs.begin(), letterLogs.end(),
+        [&](string &a, string &b) {
+            string contentA = findContent(a);
+            string contentB = findContent(b);
+            if(contentA == contentB) 
+                return findKey(a) < findKey(b);
+            else 
+                return contentA < contentB;
+        });
+
+        // O(N)
+        vector<string> res;
+        for(string &letterLog: letterLogs) 
+            res.push_back(letterLog);
+
+        // O(N)
+        for(string &log: logs) {
+            if(!isLetterLog(log))
+                res.push_back(log);
+        }
+
+        return res;
+
+    }
+};
+
+---------------my C++ solution--------------
+
 class Solution {
 private:
    bool isLetter(string &logA)
