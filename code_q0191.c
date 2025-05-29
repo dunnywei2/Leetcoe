24b64e6456e683a0716bbe525f1b337379d043a2
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2025-05-28 17:42:25 -0700

    Update code_q0191.c
    end of q0191.c

--- code_q0191.c
+++ code_q0191.c
@@ -1,16 +1,17 @@
-int main()
-{
-  int16_t input=0000000000000011b;
-  int counter=0;
-  for(int i=input;input !=0;i++)
-  {
-    if(input!=0)
+#define OFFSET 0x01U
+int hammingWeight(int n) {
+    
+    
+    int input=n;
+    int counter=0;
+    for(int i=input;input !=0;i++)
     {
-      counter++;
+        if(input&OFFSET!=0)
+        {
+            counter++;
+        }
+        input>>=1;
     }
-    input>>=1;
-  }
-  printf("counter is %d \n",counter);
-      
-
+    printf("counter is %d \n",counter);
+    return counter;   
 }
