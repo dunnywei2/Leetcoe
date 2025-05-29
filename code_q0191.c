98c5ebabf51aeb510d9137476a3060603499331a
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2025-05-28 17:21:44 -0700

    Update code_q0191.c
    code_q0191 continue

--- code_q0191.c
+++ code_q0191.c
@@ -1,10 +1,16 @@
 int main()
 {
   int16_t input=0000000000000011b;
+  int counter=0;
   for(int i=input;input !=0;i++)
   {
-
+    if(input!=0)
+    {
+      counter++;
+    }
+    input>>=1;
   }
+  printf("counter is %d \n",counter);
       
 
 }
